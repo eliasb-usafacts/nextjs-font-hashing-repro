53626f9aea4d0fae331d789ab034e4f4a7bac581
Elias Baldwin <eliasb@usafacts.org>
2024-10-23 15:34:41 -0700

    add `disableFontFamilyHashing`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,14 @@ const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
+  disableFontFamilyHashing: true,
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
+  disableFontFamilyHashing: true,
 });
 
 export const metadata: Metadata = {
